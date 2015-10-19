ceb726834c590d3652b121bcefdbc134212214ea
J Jesús Camacho E <jcamacho@itecnosolution.com>
2015-10-19 14:30:05 -0500

    Create cat_menu.php

---
+++ cat_menu.php
@@ -0,0 +1,20 @@
+<div class="block block-blog">
+	<div class="block-content">
+	<?php
+	$collection = Mage::getModel('blog/cat')->getCollection()->addStoreFilter(Mage::app()->getStore()->getId())->setOrder('sort_order', 'asc');
+	$home_url = Mage::helper('core/url')->getHomeUrl();
+	?>
+	<?php if ($collection->count()): ?>
+	<div class="menu-categories">
+	<h5><?php echo Mage::helper('blog')->__('Categories'); ?></h5>
+	<ul>
+	<?php  foreach ($collection as $item) : ?>
+	<li><a href="<?php echo $home_url.'blog/cat/'.$item['identifier'];?>" ><?php echo $item['title']; ?></a></li>
+	<?php
+		endforeach; 
+	?>
+	</ul>
+	</div>
+	<?php endif; ?>
+	</div>
+</div>
